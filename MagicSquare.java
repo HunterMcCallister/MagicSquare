a310ee302e2830d5058bd3c6855058844559d597
Hunter McCallister <huntermccalliste@u.boisestate.edu>
2024-08-27 15:02:11 -0600

    Testing tester

--- MagicSquare.java
+++ MagicSquare.java
@@ -2,6 +2,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class MagicSquare implements MagicSquareInterface {
@@ -16,7 +17,7 @@ public class MagicSquare implements MagicSquareInterface {
         File matrixFile = new File(filename);
         Scanner fileScan = new Scanner(matrixFile);
         int dimension = fileScan.nextInt();
-        int[][]matrix = new int[dimension][dimension];
+        int[][] matrix = new int[dimension][dimension];
 
         for (int row = 0; row < dimension; row++) {
             for (int col = 0; col < dimension; col++) {
@@ -29,57 +30,117 @@ public class MagicSquare implements MagicSquareInterface {
         return matrix;
 
     }
-     public MagicSquare(String filename, int dimension) throws IOException{
+
+    public MagicSquare(String filename, int dimension) throws IOException {
         int n = dimension;
         int[][] matrix = new int[n][n];
         int row = n - 1;
         int col = n / 2;
         int oldRow;
         int oldCol;
-        for(int i = 1; i <= dimension * dimension; i++){
+        for (int i = 1; i <= dimension * dimension; i++) {
             matrix[row][col] = i;
             oldRow = row;
             oldCol = col;
             row--;
             col++;
-            if (row < 0){
+            if (row < 0) {
                 row = n - 1;
             }
-            if (col >= n){
+            if (col >= n) {
                 col = 0;
             }
-            if (matrix[row][col] != 0){
+            if (matrix[row][col] != 0) {
                 row = oldRow + 1;
                 col = oldCol;
             }
         }
 
+        writeMatrix(matrix, filename);
 
-     }
-     private void writeMatrix(int[][] matrix, String filename) throws IOException{
-        
-     }
+    }
+
+    private void writeMatrix(int[][] matrix, String filename) throws IOException {
+        File readFile = new File(filename);
+        PrintWriter pw = new PrintWriter(readFile);
+        int n = matrix.length;
+        pw.println(n);
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                pw.print(matrix[row][col]);
+                if (col < n - 1) {
+                    pw.print(" ");
+                }
+            }
+            pw.println();
+        }
+        pw.close();
+
+    }
 
     @Override
     public boolean isMagicSquare() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        int targetSum = 0;
+        for (int col = 0; col < matrix.length; col++) {
+            targetSum += matrix[0][col];
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            int rowSum = 0;
+            int colSum = 0;
+
+            for (int j = 0; j < matrix.length; j++) {
+                rowSum += matrix[i][j];
+                colSum += matrix[j][i];
+            }
+            if (rowSum != targetSum || colSum != targetSum) {
+                return false;
+            }
+        }
+        int diagSum1 = 0;
+        int diagSum2 = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            diagSum1 += matrix[i][i];
+            diagSum2 += matrix[i][matrix.length - i - 1];
+        }
+        if (diagSum1 != targetSum || diagSum2 != targetSum) {
+            return false;
+        }
+        return true;
+
     }
 
     @Override
     public int[][] getMatrix() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        int n = matrix.length;
+        int[][] matrixCopy = new int[n][n];
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                matrixCopy[i][j] = matrix [i][j];
+            }
+        }
+        return matrixCopy;
     }
 
     @Override
     public String toString() {
-        String matrixString = "The Matrix" + "\n";
-        for (int i = 0; i < matrix.length; i++){
-            for (int j = 0; j < matrix.length; j++){
-                matrixString = matrix[i][j] + "";
+        String matrixString = "The Matrix:\n";
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrixString = matrix[i][j] + " ";
             }
             matrixString += "\n";
+        }
+        if (isMagicSquare()) {
+            matrixString += "is a magic square.";
+        } else {
+            matrixString += "is not a magic square.";
+        }
+        return matrixString;
         }
 
     }
 
-}
+
--- MagicSquareDriver.java
+++ MagicSquareDriver.java
@@ -1,4 +1,6 @@
+
 public class MagicSquareDriver {
+
     public static void main(String[] args) {
         String usageMessage = "Usage: java MagicSquareDriver <-check | -create> <filename> [size]";
 
@@ -7,14 +9,31 @@ public class MagicSquareDriver {
             return;
         }
 
-        String arg = args[0];
-        String file = args[1];
-        int length = args.length;
-        
-        
-        if (!arg.equals("-check") && !arg.equals("-create")) {
+        String option = args[0];
+        String filename = args[1];
+
+        if (!option.equals("-check") && !option.equals("-create")) {
             System.out.println(usageMessage);
-            return;            
+            return;
+        }
+
+        try {
+            if (option.equals("-check")) {
+                MagicSquare magicSquare = new MagicSquare(filename);
+                String result = magicSquare.isMagicSquare() ? "is a magic square." : "is not a magic square.";
+                System.out.println("The matrix in " + filename + " " + result);
+            } else if (option.equals("-create")) {
+                if (args.length != 3) {
+                    System.out.println(usageMessage);
+                    return;
+                }
+
+                int size = Integer.parseInt(args[2]);
+                MagicSquare magicSquare = new MagicSquare(filename, size);
+                System.out.println("Created a " + size + "x" + size + " magic square in " + filename);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
